4c45f9db67bad63a6a1306201c897e732edfd187
JoseJoseQuispeDavila <023101527i@uandina.edu.pe>
2025-05-18 23:43:30 -0500

    Add files via upload

---
+++ Taller 3.1 AlgebraRelacional/WebForm1.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="AlgebraRelacional.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <!-- Título general -->
+            <asp:Label ID="lblTitulo" runat="server" Text="Operaciones Unarias" Font-Bold="true" Font-Size="X-Large" /><br /><br />
+
+            <!-- Selección (σ) -->
+            <asp:Label ID="lblSeleccion" runat="server" Text="Selección (σ)" Font-Bold="true" Font-Size="Large" /><br />
+            <asp:Button Text="Selección 1: Tabla Producto " runat="server" ID="btnSeleccion1" OnClick="btnSeleccion1_Click" />
+            <asp:Button Text="Selección 2: Tabla Vendedor" runat="server" ID="btnSeleccion2" OnClick="btnSeleccion2_Click" />
+            <asp:Button Text="Selección 3: Tabla Detalle" runat="server" ID="btnSeleccion3" OnClick="btnSeleccion3_Click" />
+            <br /><br />
+
+            <!-- Proyección (π) -->
+            <asp:Label ID="lblProyeccion" runat="server" Text="Proyección (π)" Font-Bold="true" Font-Size="Large" /><br />
+            <asp:Button Text="Proyección 1: Nombres y Apellidos de Clientes" runat="server" ID="btnProyeccion1" OnClick="btnProyeccion1_Click" />
+            <asp:Button Text="Proyección 2: Códigos y Precios de Productos" runat="server" ID="btnProyeccion2" OnClick="btnProyeccion2_Click" />
+            <asp:Button Text="Proyección 3: Categorías de Productos" runat="server" ID="btnProyeccion3" OnClick="btnProyeccion3_Click" />
+            <br /><br />
+
+            <!-- Renombramiento (ρ) -->
+            <asp:Label ID="lblRenombramiento" runat="server" Text="Renombramiento (ρ)" Font-Bold="true" Font-Size="Large" /><br />
+            <asp:Button Text="Renombramiento 1: Renombrar columnas de la tabla Categoria" runat="server" ID="btnRenombramiento1" OnClick="btnRenombramiento1_Click" />
+            <asp:Button Text="Renombramiento 2: Renombrar columnas de la tabla Producto" runat="server" ID="btnRenombramiento2" OnClick="btnRenombramiento2_Click" />
+            <asp:Button Text="Renombramiento 3: Renombrar columnas de la tabla Cliente" runat="server" ID="btnRenombramiento3" OnClick="btnRenombramiento3_Click" />
+            <br /><br />
+
+            <!-- Operaciones Binarias -->
+            <asp:Label ID="lblBinarias" runat="server" Text="Operaciones Binarias" Font-Bold="true" Font-Size="X-Large" /><br /><br />
+
+            <!-- Unión (∪) -->
+            <asp:Label ID="lblUnion" runat="server" Text="Unión (∪)" Font-Bold="true" Font-Size="Large" /><br />
+            <asp:Button Text="Unión 1: Uniendo nombres de clientes y vendedores" runat="server" ID="btnUnion1" OnClick="btnUnion1_Click"  />
+            <asp:Button Text="Unión 2: Unión de vendedores por inicio de nombre/apellido." runat="server" ID="btnUnion2" OnClick="btnUnion2_Click" />
+            <asp:Button Text="Unión 3: Unión de categorías Calzado y Embutidos" runat="server" ID="btnUnion3" OnClick="btnUnion3_Click" />
+            <br /><br />
+
+            <!-- Diferencia (−) -->
+            <asp:Label ID="lblDiferencia" runat="server" Text="Diferencia (−)" Font-Bold="true" Font-Size="Large" /><br />
+            <asp:Button Text="Diferencia 1: Productos que no están en detalles de boletas/pedidos" runat="server" ID="btnDiferencia1" OnClick="btnDiferencia1_Click" />
+            <asp:Button Text="Diferencia 2: Productos que no están en detalles de boletas/pedidos" runat="server" ID="btnDiferencia2" OnClick="btnDiferencia2_Click" />
+            <asp:Button Text="Diferencia 3: Clientes que nunca han comprado (no tienen boletas)" runat="server" ID="btnDiferencia3" OnClick="btnDiferencia3_Click" />
+            <br /><br />
+
+            <!-- Producto Cartesiano (×) -->
+            <asp:Label ID="lblProducto" runat="server" Text="Producto Cartesiano (×)" Font-Bold="true" Font-Size="Large" /><br />
+            <asp:Button Text="Producto 1: Producto Cartesiano: Clientes x Boletas" runat="server" ID="btnProducto1" OnClick="btnProducto1_Click" />
+            <asp:Button Text="Producto 2: Producto Cartesiano: Detalles x Boletas" runat="server" ID="btnProducto2" OnClick="btnProducto2_Click" />
+            <asp:Button Text="Producto 3: Producto Cartesiano: Vendedores x Boletas" runat="server" ID="btnProducto3" OnClick="btnProducto3_Click" />
+            <br /><br />
+
+            <!-- Resultado -->
+<asp:GridView runat="server" ID="gvConsulta"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Taller 3.1 AlgebraRelacional/WebForm1.aspx.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AlgebraRelacional
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSeleccion1_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from P in ventas.TProducto
+                           select P;
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnSeleccion2_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from P in ventas.TVendedor
+                           select P;
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnSeleccion3_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from P in ventas.TDetalle
+                           select P;
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnProyeccion1_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from C in ventas.TCliente
+                           select new { C.Nombres, C.Apellidos, C.Direccion };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnProyeccion2_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from T in ventas.TProducto
+                           select new { T.CodProduto, T.Precio };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnProyeccion3_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from C in ventas.TCategoria
+                           select new { C.CodCategoria, C.Nombre };
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnRenombramiento1_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from C in ventas.TCategoria
+                           select new
+                           {
+                               IdentificadorCategoria = C.CodCategoria,
+                               NombreDeCategoria = C.Nombre
+                           };
+
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnRenombramiento2_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from P in ventas.TProducto
+                           select new
+                           {
+                               CodigoProducto = P.CodProduto,
+                               NombreProducto = P.Nombre,
+                               PrecioUnitario = P.Precio,
+                               CantidadEnStock = P.Stock,
+                               CodigoDeCategoria = P.CodCategoria
+                           };
+
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnRenombramiento3_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from CL in ventas.TCliente
+                           select new
+                           {
+                               NumeroCliente = CL.CodCliente,
+                               NombresDelCliente = CL.Nombres,
+                               ApellidosDelCliente = CL.Apellidos,
+                               DireccionDelCliente = CL.Direccion
+                           };
+
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnUnion1_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+
+            var nombresClientes = from C in ventas.TCliente
+                                  select new { NombreCompleto = C.Nombres + " " + C.Apellidos };
+
+            var nombresVendedores = from V in ventas.TVendedor
+                                    select new { NombreCompleto = V.Nombres + " " + V.Apellidos };
+
+            var todosLosNombres = nombresClientes.Union(nombresVendedores).ToList();
+
+            gvConsulta.DataSource = todosLosNombres;
+            gvConsulta.DataBind();
+        }
+
+        protected void btnUnion2_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+
+            var vendedoresConS = from V in ventas.TVendedor
+                                 where V.Nombres.StartsWith("S")
+                                 select new { NombreCompleto = V.Nombres + " " + V.Apellidos };
+
+            var vendedoresConP = from VE in ventas.TVendedor
+                                 where VE.Apellidos.StartsWith("P")
+                                 select new { NombreCompleto = VE.Nombres + " " + VE.Apellidos };
+
+            var vendedoresSyP = vendedoresConS.Union(vendedoresConP).ToList();
+
+            gvConsulta.DataSource = vendedoresSyP;
+            gvConsulta.DataBind();
+        }
+
+        protected void btnUnion3_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+
+            var categoriasCalzado = from C in ventas.TCategoria
+                                    where C.Nombre == "Calzado"
+                                    select new { NombreCategoria = C.Nombre };
+
+            var categoriasEmbutidos = from CA in ventas.TCategoria
+                                      where CA.Nombre == "Embutidos"
+                                      select new { NombreCategoria = CA.Nombre };
+
+            var categoriasCalzadoYEmbutidos = categoriasCalzado.Union(categoriasEmbutidos).ToList();
+
+            gvConsulta.DataSource = categoriasCalzadoYEmbutidos;
+            gvConsulta.DataBind();
+        }
+
+        protected void btnDiferencia1_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from p in ventas.TProducto
+                           where !(ventas.TDetalle.Select(d => d.CodProducto).Contains(p.CodProduto))
+                           select p;
+
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnDiferencia2_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+
+            var consulta = from c in ventas.TCategoria
+                           where !(ventas.TProducto.Select(p => p.CodCategoria).Contains(c.CodCategoria))
+                           select c;
+
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnDiferencia3_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+
+            var consulta = from c in ventas.TCliente
+                           where !(ventas.TBoleta.Select(b => b.CodCliente).Contains(c.CodCliente))
+                           select c;
+
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnProducto1_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from cliente in ventas.TCliente
+                           from boleta in ventas.TBoleta
+                           select cliente;
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnProducto2_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from detalle in ventas.TDetalle
+                           from boleta in ventas.TBoleta
+                           select detalle;
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+
+        protected void btnProducto3_Click(object sender, EventArgs e)
+        {
+            BDVentasEntities ventas = new BDVentasEntities();
+            var consulta = from vendedor in ventas.TVendedor
+                           from boleta in ventas.TBoleta
+                           select vendedor;
+            gvConsulta.DataSource = consulta.ToList();
+            gvConsulta.DataBind();
+        }
+    }
+}
